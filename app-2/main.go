6ea39ee69264b21d873c859162114af85a32486b
Ehsan Rassekh <rassekh.ehsan@gmail.com>
2021-05-23 13:16:35 +0430

    adding app-2

---
+++ app-2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name string = "https://iasbs.ac.ir/~rassekh.ehsan/"
+	name := "Ehsan Rassekh"
+	name = "Rassekh"
+
+	fmt.Println(name)
+	fmt.Println("1234")
+}
